db019301b05f52bad0a0cab1a0b62d84d9806cc8
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-11 11:52:19 -0500

    adds don't send messages to self feature

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const app = require ('./app');
-const {PORT, NODE_ENV} = require('./config');
+const app = require('./app');
+const { PORT, NODE_ENV } = require('./config');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
@@ -7,22 +7,22 @@ let users = {};
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.broadcast.emit('userconnect','new user connected');
+  socket.broadcast.emit('userconnect', 'new user connected');
   socket.on('username save', (username) => {
     console.log(socket.id);
-    users[socket.id]=username;
+    users[socket.id] = username;
     socket.emit('username saved', `${users[socket.id]} saved`);
   });
   socket.on('disconnect', () => {
-    io.emit('disconnect','user disconnected');
+    io.emit('disconnect', 'user disconnected');
     console.log('user disconnected');
   });
   socket.on('chat message', (msg) => {
-    if(socket.id in users){
-      io.emit('chat message',`${users[socket.id]}: ${msg}`);
-    }else{
-      users[socket.id]=socket.id;
-      io.emit('chat message',`${users[socket.id]}: ${msg}`);
+    if (socket.id in users) { //if they have a username
+      socket.broadcast.emit('chat message', `${users[socket.id]}: ${msg}`);
+    } else { //if they don't have a username
+      users[socket.id] = socket.id;
+      socket.broadcast.emit('chat message', `${users[socket.id]}: ${msg}`);
     }
     console.log('message: ' + msg);
   });
